7743579b25a05690838db2cfb5dfae1bc71c733a
Corey Robinson <c7o7r7e7y@gmail.com>
2015-03-26 11:04:43 -0400

    New Assets

---
+++ Assets/scripts/CloudSpawner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cloudspawner : MonoBehaviour {
+
+	public GameObject cloud;
+	public Vector3 position;
+
+	// Use this for initialization
+	void Start () {
+
+		Instantiate(cloud, spawnLocation, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
