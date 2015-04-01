164a372b00a7b53789a0a681ad567be1384d6f85
leoantelyes <lantelyes@gmail.com>
2015-04-01 16:09:47 -0400

    More cloud

--- Assets/Scripts/Cloud.cs
+++ Assets/Scripts/Cloud.cs
@@ -3,15 +3,23 @@ using System.Collections;
 
 public class Cloud : MonoBehaviour {
 
+
+	Vector3 origPos;
 	// Use this for initialization
 	void Start () {
 	
+		origPos = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		transform.position += new Vector3 (-10.0f * Time.deltaTime, 0, 0);
+		transform.position += new Vector3 (-5.0f * Time.deltaTime, 0, 0);
+
+		if (transform.position.x <= -25) {
+			transform.position = origPos + new Vector3(0,Random.Range(-5,5));
+			transform.localScale.Scale(new Vector3(Random.Range(0.75f,1.25f),Random.Range(0.75f,1.25f),Random.Range(0.75f,1.25f)));
+		}
 	
 	}
 }
