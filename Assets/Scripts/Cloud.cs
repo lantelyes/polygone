f0711e4417232f4d4bffc40146079af60f218f79
leoantelyes <lantelyes@gmail.com>
2015-04-01 16:02:47 -0400

    cloud stuff

---
+++ Assets/Scripts/Cloud.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cloud : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position += new Vector3 (-10.0f * Time.deltaTime, 0, 0);
+	
+	}
+}
