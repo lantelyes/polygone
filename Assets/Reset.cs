fe6326158b0a7e4dde6bfa917b0844db63983673
leoantelyes <lantelyes@gmail.com>
2015-05-13 15:12:35 -0400

    Final updates

---
+++ Assets/Reset.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Reset : MonoBehaviour {
+
+	public GameObject HighScoreObject;
+
+	// Use this for initialization
+	void Start () {
+
+	
+
+	
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown (0)) {
+			Application.LoadLevel ("new_menu");
+		}
+	
+	}
+}
